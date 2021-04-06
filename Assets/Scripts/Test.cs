4b1514c2f05e351401a56fa059d78c2d940db7b9
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2021-04-07 00:58:10 +0800

    Added game object

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
     public int helloThere;
+
+    public GameObject gameObject;
+
     // Start is called before the first frame update
     void Start()
     {
